4fd649dc2822818f486de18ea89506ccc217039b
Merve OKTAY <merve.oktay@hotmail.com>
2025-01-05 23:10:36 +0300

    Screens design

--- app/src/main/java/com/example/feedthesnake/ui/presentation/GameScreen.kt
+++ app/src/main/java/com/example/feedthesnake/ui/presentation/GameScreen.kt
@@ -6,12 +6,9 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,7 +26,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.feedthesnake.R
-import com.example.feedthesnake.ui.components.CustomButton
 import com.example.feedthesnake.ui.theme.DarkGreen
 import com.example.feedthesnake.ui.theme.DarkGrey
 import com.example.feedthesnake.ui.theme.Green
